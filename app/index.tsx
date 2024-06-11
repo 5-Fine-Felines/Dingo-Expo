13fc3e14ccca272c512da7c5084d6db1bc024f76
Suneth Udayanga <sunethuw@gmail.com>
2024-06-11 21:15:28 +0530

    update
    Co-Authored-By: Asma Rafeek <91976475+Asmarfk@users.noreply.github.com>
    Co-Authored-By: Charitha Bandara <56010136+ChRithVI@users.noreply.github.com>
    Co-Authored-By: Oshan Kalhara <91822308+MAOKalhara@users.noreply.github.com>
    Co-Authored-By: Ushmika Kavishan <91392668+Ushmika@users.noreply.github.com>
    Co-Authored-By: dinujakelum <118272584+dinujakelum@users.noreply.github.com>
    Co-Authored-By: rashmika3433 <65844409+rashmika3433@users.noreply.github.com>

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='flex-1 bg-color-red'>
+      <Text className='text text-xl font-bold text-green-700'>index</Text>
+      <Text className='text text-xl font-bold text-green-700'>index2</Text>
+    </View>
+  )
+}
+
+export default index;
