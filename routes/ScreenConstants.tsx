080aab3e69dd752c05d61570fde9e18ab1697245
Charitha Bandara <charithac.cb@gmail.com>
2024-06-14 16:16:20 +0530

    need to merge main branch

--- routes/ScreenConstants.tsx
+++
@@ -1,8 +0,0 @@
-export const SCREENS = {
-    SPLASH: 'SPLASH',
-    ONBOARDING: 'ONBOARDING',
-    AUTH: 'LOGIN',
-    REGISTER: 'REGISTER',
-    USERHOME: 'USERHOME',
-    USERPROFILE: 'USERPROFILE',
-  } as const;
